8d5738516244824d0c10a099d7cf9246dc294942
Miguel Guerrero <seetalkit@gmail.com>
2017-04-14 15:23:47 -0400

    Add process ENV

--- db/index.js
+++ db/index.js
@@ -1,9 +1,25 @@
 var mysql      = require('mysql');
+
+var dataSource = '';
+var userId = '';
+var password = '';
+var database = '';
+
+const connStr = process.env.MYSQLCONNSTR_awsConnection;
+if(connStr) {
+  const connArr = connStr.split(';');
+  const connExtended = connArr.map(str => str.split('='))
+  database = connExtended[0][1];
+  dataSource = connExtended[1][1];
+  userId = connExtended[2][1];
+  password = connExtended[3][1];
+}
+
 var connection = mysql.createConnection({
-  host     : '',
-  user     : '',
-  password : '',
-  database : ''
+ host     : dataSource,
+ user     : userId,
+ password : password,
+ database : database
 });
 
 module.exports = connection;
