5235894c830053ec9a9fbf3f6aacab60885d1a5e
Miguel Guerrero <seetalkit@gmail.com>
2017-05-13 15:45:41 -0400

    Change pin number

--- conf/serverConf.js
+++ conf/serverConf.js
@@ -3,7 +3,7 @@ var ONE_DAY = 86400000; // In milliseconds
 var Config = {
     ASSETS_MAX_AGE: 7*ONE_DAY,
     PAGE_MAX_AGE: 10*ONE_MIN,   // As discussed kept it 10 mins. You can change it of you want to cache the queries for longer time.
-    SECRET_PIN: "0412",           // Change this PIN to some secret value
+    SECRET_PIN: "6942",           // Change this PIN to some secret value
     COOKIE_NAME: '_tabupin',
     COOKIE_AGE: ONE_DAY,
 
